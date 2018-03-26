fe88c4e8eebdb921698f81b58b9826fc2eaf1335
James Buchanan Young <37392094+rubyautomation@users.noreply.github.com>
2018-03-26 23:59:45 +1100

    Make it clear this is in a WordPress directory.

--- getpassword.php
+++ getpassword.php
@@ -1,6 +1,6 @@
 <?php
 
-	include_once '/var/www/html/afdc/wp-includes/class-phpass.php';
+	include_once '/var/www/some-wordpress-site/wp-includes/class-phpass.php';
 
 	$p = new PasswordHash(8, TRUE);
 	$hashed = $p->HashPassword($_GET['p']);
